9e403690a958b44d573fc4fd811884af75ac684d
Nagaranjan-r-de <nagaranjan.r.de@gmail.com>
2026-08-31 20:33:49 +0530

    Update portfolioData.js

--- frontend/src/data/portfolioData.js
+++ frontend/src/data/portfolioData.js
@@ -15,9 +15,17 @@ const portfolioData = {
       id: "p1",
       name: "wildlife movement analysis and conservation monitoring system.",
       description: "Using demo data, predicts behavior, detects anomalies, and provides conservation risk insights through an interactive dashboard.",
-      image: "/projects/project1.png",
+      image: "/projects/project1.jpeg",
       demoUrl: "https://wild-movement-analysis.netlify.app",
       repoUrl: "https://github.com/nagaranjan-r-de/wildlife-movement-analysis/tree/main",
+    },
+    {
+      id: "p2",
+      name: "personal-money-manager",
+      description: "It helps my finances by setting daily and monthly spending limits and monitoring the expenses.",
+      image: "/projects/project2.jpeg",
+      demoUrl: "person use only",
+      repoUrl: "https://github.com/nagaranjan-r-de/personal-money-management",
     },
   ],
   certificates: [
